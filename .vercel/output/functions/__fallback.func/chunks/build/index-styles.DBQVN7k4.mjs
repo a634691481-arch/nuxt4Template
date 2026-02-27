bf30ebe7d0b5fb7f1bce88ebcaef7fba8ecc1675
yangliu <634691481@qq.com>
2026-02-27 15:34:18 +0800

    chore(vercel): 删除无用的 Vercel 输出文件和配置
    - 清理 .vercel/output/config.json 文件
    - 删除 __fallback 函数相关的配置与代码文件
    - 移除多余的源码映射及事件处理相关代码文件
    - 减少冗余文件，优化部署输出目录结构

--- .vercel/output/functions/__fallback.func/chunks/build/index-styles.DBQVN7k4.mjs
+++
@@ -1,8 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_14302432_lang = ".bg-img[data-v-14302432]{background-image:linear-gradient(0deg,#000000b3,#0000004d)}.masonry-item[data-v-14302432],[data-v-14302432] .masonry-item{position:relative!important}";
-
-const indexStyles_DBQVN7k4 = [
-  index_vue_vue_type_style_index_0_scoped_14302432_lang
-];
-
-export { indexStyles_DBQVN7k4 as default };
-//# sourceMappingURL=index-styles.DBQVN7k4.mjs.map
--- .vercel/output/functions/__fallback.func/chunks/build/useDirection-B67GYiMj.mjs
+++
@@ -1,9 +0,0 @@
-import { a7 as injectConfigProviderContext, v as vueExports } from './server.mjs';
-
-function useDirection(dir) {
-  const context = injectConfigProviderContext({ dir: vueExports.ref("ltr") });
-  return vueExports.computed(() => dir?.value || context.dir?.value || "ltr");
-}
-
-export { useDirection as u };
-//# sourceMappingURL=useDirection-B67GYiMj.mjs.map
